f00ef3d187b0038dabb7fa9bc88a8990e0650497
Rodrigo <rodrigo.boas@etec.sp.gov.br>
2025-11-07 13:06:14 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaReinoDoce/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaReinoDoce
+{
+    internal class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public int QuantidadeEstoque { get; set; }
+        public DateTime DataValidade { get; set; }
+
+
+      
+    }
+}
---
+++ SistemaReinoDoce/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SistemaReinoDoce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
